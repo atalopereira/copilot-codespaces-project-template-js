91a8fde3f78cce22a9ea950f938c4d7e7f9b2b74
Átalo Pereira <atalopereira01@gmail.com>
2025-03-10 19:50:24 +0000

    Atualiza informações pessoais e links no projeto, incluindo nome, e-mail e descrição, além de modificar imagens e títulos de projetos no portfólio.

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,15 +16,15 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/fundo-bkg.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
-const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+const description = 
+  "I am a passionate UI/UX designer with a focus on creating inclusive and accessible digital experiences. With a background in web design and user research, I strive to make technology accessible to all users.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -44,8 +44,8 @@ const skillsList = [
  * how you best work, or even a quote. This will help someone learn more
  * about you on a professional level.
  */
-const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+const detailOrQuote = 
+  "I believe in the power of design to create positive change and improve people's lives. My goal is to make technology more accessible and user-friendly for everyone.";
 
 const About = () => {
   return (
